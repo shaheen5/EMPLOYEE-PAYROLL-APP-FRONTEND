c4f336614f2391b881c8624656cc609e3b7d8ed7
shaheen5 <miyashaheen1995@gmail.com>
2021-07-07 13:42:31 +0530

    feat : design login page UI using react material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Register } from './pages/register'
+import {Login } from './pages/login'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,13 @@ function App() {
       <Switch>
         <Route path='/registerUser'>
         <Register />
+        </Route>
+      </Switch>
+      </BrowserRouter>
+      <BrowserRouter>
+      <Switch>
+        <Route path='/login'>
+        <Login />
         </Route>
       </Switch>
       </BrowserRouter>
