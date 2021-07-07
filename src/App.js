d78eee9d800400c2148e0af667f49fdae498f34a
shaheen5 <miyashaheen1995@gmail.com>
2021-07-07 11:46:21 +0530

    feat : add routes to registration page using React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import {Register} from './pages/register'
+import { Register } from './pages/register'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App" >
-      <Register/>
+      <h1>EMPLOYEE PAYROLL APP</h1>
+      <BrowserRouter>
+      <Switch>
+        <Route path='/registerUser'>
+        <Register />
+        </Route>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
