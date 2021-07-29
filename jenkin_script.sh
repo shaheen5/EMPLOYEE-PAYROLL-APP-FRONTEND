91d1207ae0f882e671c969d97a9491f3423a05e5
shaheen5 <miyashaheen1995@gmail.com>
2021-07-29 13:31:27 +0530

    refactor : updated jenkins script file

--- jenkin_script.sh
+++ jenkin_script.sh
@@ -1,3 +1,8 @@
-cd /home/ubuntu/Employee Payroll App FrontEnd
+#!/bin/bash -x
+
+cd /home/ubuntu/Employee_Payroll_App_FrontEnd
+working_directory=$(pwd)
+echo "Present working directory = $working_directory"
 npm i 
+echo "Installing packages"
 npm start
