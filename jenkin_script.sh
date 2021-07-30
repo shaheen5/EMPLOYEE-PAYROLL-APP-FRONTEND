d12113acec90c5e2cfcc775f6def02b2dc9a0680
shaheen5 <miyashaheen1995@gmail.com>
2021-07-30 19:42:50 +0530

    refactor : change script file ,install webpack package

--- jenkin_script.sh
+++ jenkin_script.sh
@@ -4,6 +4,7 @@ cd /home/ubuntu/Employee_Payroll_App_FrontEnd
 working_directory=$(pwd)
 echo "Present working directory = $working_directory"
 npm i 
+npm install node-sass@6.0.1
 echo "Installing packages"
 npx kill-port 3000
 npm start
