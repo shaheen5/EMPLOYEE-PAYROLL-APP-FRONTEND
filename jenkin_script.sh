83c667567996fbb114f9d89745a4f6a66906eb04
shaheen5 <miyashaheen1995@gmail.com>
2021-07-30 20:22:37 +0530

    refactor : add packages for node-saas and popperjs

--- jenkin_script.sh
+++ jenkin_script.sh
@@ -4,7 +4,6 @@ cd /home/ubuntu/Employee_Payroll_App_FrontEnd
 working_directory=$(pwd)
 echo "Present working directory = $working_directory"
 npm i 
-npm install node-sass@6.0.1
 echo "Installing packages"
 npx kill-port 3000
 npm start
