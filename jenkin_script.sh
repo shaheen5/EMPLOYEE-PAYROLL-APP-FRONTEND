c07b5690e4b662f2da01af320af4326a91a87571
shaheen5 <miyashaheen1995@gmail.com>
2021-07-30 21:56:26 +0530

    refactor : upgrade webpack version on and change jenkin script

--- jenkin_script.sh
+++ jenkin_script.sh
@@ -4,6 +4,7 @@ cd /home/ubuntu/Employee_Payroll_App_FrontEnd
 working_directory=$(pwd)
 echo "Present working directory = $working_directory"
 npm i 
+npm -g i sass
 echo "Installing packages"
 npx kill-port 3000
 npm start
