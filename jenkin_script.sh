18a4b872a929832ab621c1e34c5352ff278b8237
shaheen5 <miyashaheen1995@gmail.com>
2021-07-31 21:42:59 +0530

    refactor : update jenkins script file

--- jenkin_script.sh
+++ jenkin_script.sh
@@ -4,8 +4,6 @@ cd /home/ubuntu/Employee_Payroll_App_FrontEnd
 working_directory=$(pwd)
 echo "Present working directory = $working_directory"
 npm i 
-npm i sass
-npm install webpack
 echo "Installing packages"
 npm run build
 echo "build success!"
