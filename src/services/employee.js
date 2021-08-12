a1676b0f3b23f189f6091bcb8bec7c906997a929
shaheen5 <miyashaheen1995@gmail.com>
2021-08-12 23:39:48 +0530

    refactor : remove code redundancy for headers in api call request

--- src/services/employee.js
+++ src/services/employee.js
@@ -1,47 +1,47 @@
+/** ******************************************************************************
+ *  Execution    : 1. Default node with npm   cmd> npm start
+ *
+ * Purpose      : Employee CRUD API Integration
+ *
+ * @description  :modules need to be required before execution of this file
+ *
+ * @file        : services/employee.js
+ * @overview    : api integration using axios
+ * @module      : Contains code to send api request to backend 
+ * @author      : Shaheen M.
+ * @version     : 1.0
+ * @since       : 19-07-2021
+ ********************************************************************************/
 import Axios from "axios";
 Axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 const token = localStorage.getItem("token");
+const header = {
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+}
 
 export class Employee {
+
   createEmployee = (employeeDetails) => {
-    return Axios.post("/employees", employeeDetails, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    return Axios.post("/employees", employeeDetails, header);
   };
 
-  getAllEmployees = ()=>{
-    return Axios.get("/getEmployees",{
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+  getAllEmployees = () => {
+    return Axios.get("/getEmployees", header);
   }
 
-  editEmployee = (empData,employeeId)=>{
+  editEmployee = (empData, employeeId) => {
     return Axios.put(`/updateEmployee/${employeeId}`,
-    empData,{
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+      empData, header);
   }
 
-  getEmployee = (employeeId)=>{
-    return Axios.get(`/getEmployee/${employeeId}`,{
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+  getEmployee = (employeeId) => {
+    return Axios.get(`/getEmployee/${employeeId}`, header);
   }
 
-  removeEmployee = (employeeId)=>{
-    return Axios.delete(`/deleteEmployee/${employeeId}`,{
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+  removeEmployee = (employeeId) => {
+    return Axios.delete(`/deleteEmployee/${employeeId}`, header);
   }
 
 }
